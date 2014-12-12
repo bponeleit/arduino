47e1b627b51ea57a815440bdf07a7c5c4c871603
Burkart Poneleit <burkart.poneleit@gmail.com>
2014-12-12 22:13:33 +0100

    Changed to API mode
    Use library xbee-arduino and API mode 2
    String/char[] is currently a mess

--- Sensor_Node_1/Sensor_Node_1.ino
+++ Sensor_Node_1/Sensor_Node_1.ino
@@ -3,6 +3,7 @@
 
 #include "DHT.h"
 #include <JeeLib.h>
+#include <XBee.h>
 
 ISR(WDT_vect) { 
   Sleepy::watchdogEvent(); 
@@ -23,34 +24,136 @@ ISR(WDT_vect) {
 // Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
 
 DHT dht(DHTPIN, DHTTYPE);
+XBee xbee = XBee();
+
+uint8_t payload[] = { 
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, }; 
+
+XBeeAddress64 addr64 = XBeeAddress64(0x0013a200, 0x40acb044);
+ZBTxRequest zbTx = ZBTxRequest();//addr64, payload, sizeof(payload));
+ZBTxStatusResponse txStatus = ZBTxStatusResponse();
 
 int photocellPin = 0;     // the cell and 10K pulldown are connected to a0
 int photocellReading;     // the analog reading from the sensor divider
 
+int statusLed = 13;
+int errorLed = 13;
+
 float h;
 float t;
+
+void flashLed(int pin, int times, int wait) {
+  Serial.println("flash led");
+  for (int i = 0; i < times; i++) {
+    digitalWrite(pin, HIGH);
+    delay(wait);
+    digitalWrite(pin, LOW);
+
+    if (i + 1 < times) {
+      delay(wait);
+    }
+  }
+}
 
 void setup() {
   Serial.begin(9600); 
   //Serial.println("DHTxx test!");
 
   dht.begin();
+  xbee.setSerial(Serial);
+
+  zbTx.setAddress64(addr64);
 }
 
-void loop() {
-  //Every 3 Seconds  
-  Sleepy::loseSomeTime(3000);  
-  
+void loop() {  
+
   readDHT();
   readPhotocell();
+
+  char nodebuf[7];
+  itoa(NODE_ID, nodebuf, 10);
+
+  char tbuf[7];
+  dtostrf(t, 6, 2, tbuf);
+
+  char hbuf[7];
+  dtostrf(h, 6, 2, hbuf);
+
+  char photobuf[7];
+  itoa(photocellReading, photobuf, 10);
+
+  char space[2] = { 
+    0x20, 0x00   };
+
+  char strbuf[28];
+  strcpy(strbuf, nodebuf);
+  strcat(strbuf, space);
+  strcat(strbuf, tbuf);
+  strcat(strbuf, space);
+  strcat(strbuf, hbuf);
+  strcat(strbuf, space),
+  strcat(strbuf, photobuf);
+
+  uint8_t data[28];
+  memcpy(data, strbuf, 28);
   
-  Serial.print(NODE_ID);
-  Serial.print(" ");
-  Serial.print(t, DEC);
-  Serial.print(" ");
-  Serial.print(h, DEC);
-  Serial.print(" ");
-  Serial.println(photocellReading);
+  //Serial.print(strbuf);
+  
+
+  zbTx.setPayload(data);
+  zbTx.setPayloadLength(sizeof(data));
+  //Serial.println("Before send");
+  xbee.send(zbTx);
+  //Serial.print("After send");
+
+  //Serial.print(NODE_ID); //2 bytes
+  //Serial.print(" "); // 1 byte
+  //Serial.print(t, DEC); 
+  //Serial.print(" ");
+  //Serial.print(h, DEC);
+  //Serial.print(" ");
+  //Serial.println(photocellReading);
+  
+  flashLed(statusLed, 1, 100);
+
+  // after sending a tx request, we expect a status response
+  // wait up to half second for the status response
+  if (xbee.readPacket(1000)) {
+    // got a response!
+
+    // should be a znet tx status            	
+    if (xbee.getResponse().getApiId() == ZB_TX_STATUS_RESPONSE) {
+      xbee.getResponse().getZBTxStatusResponse(txStatus);
+
+      // get the delivery status, the fifth byte
+      if (txStatus.getDeliveryStatus() == SUCCESS) {
+        // success.  time to celebrate
+        flashLed(statusLed, 5, 200);
+        Serial.print("success");
+      } 
+      else {
+        // the remote XBee did not receive our packet. is it powered on?
+        Serial.print("Delivery failed");
+        flashLed(errorLed, 3, 200);
+      }
+    }
+  } 
+  else if (xbee.getResponse().isError()) {
+    //Serial.print("Analog reading = ");
+    Serial.print("Error reading packet.  Error code: ");  
+    Serial.println(xbee.getResponse().getErrorCode());
+  } 
+  else {
+    // local XBee did not provide a timely TX Status Response -- should not happen
+    Serial.print("XBee no response");
+    Serial.print(xbee.getResponse().getApiId());
+    flashLed(errorLed, 2, 200);
+  }
+  
+  //delay(1000);
+    //Every 3 Seconds  
+  Sleepy::loseSomeTime(30000);
+
 }
 
 void readDHT() {
@@ -63,4 +166,5 @@ void readDHT() {
 void readPhotocell() {
   photocellReading = analogRead(photocellPin);  
 }
+
 
